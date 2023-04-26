0d133ee7a8a529634edcf8ac9b4a301ed71282bf
siraj <sirajbhutta50@gmail.com>
2023-04-26 11:52:53 +0500

    Minimal python/flask api

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/mydata')
+def mydata():
+    data = [
+        {'name': 'John', 'age': 30, 'city': 'New York'},
+        {'name': 'Jane', 'age': 25, 'city': 'Los Angeles'},
+        {'name': 'Bob', 'age': 40, 'city': 'Chicago'}
+    ]
+    status_code = 200
+    response_data = (data, status_code)
+    # Adding new data to the response
+    response_data = (response_data[0], response_data[1], {'is_valid': True})
+    return jsonify(response_data)
 
 
 if __name__ == '__main__':
